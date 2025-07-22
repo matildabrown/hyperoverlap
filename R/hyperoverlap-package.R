d735fa3a02c1d399827ea73e09cd50d355273974
Matilda Brown <M.Brown2@kew.org>
2025-07-22 17:02:44 +0100

    fix ggplot e1071 import conflict, update version

--- R/hyperoverlap-package.R
+++ R/hyperoverlap-package.R
@@ -1,14 +1,18 @@
 #' Hyperoverlap: Detection and Visualisation of Overlap in n-Dimensional Space
 #'
-#' @docType package
-#' @name hyperoverlap
-#'
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @import ggplot2
+## usethis namespace: end
+NULL
+
+#' `r lifecycle::badge("stable")`
 #' @description Uses support vector machines to identify a perfectly separating hyperplane (linear or curvilinear) between two entities in high-dimensional space. If this plane exists, the entities do not overlap. Applications include overlap detection in morphological, resource or environmental dimensions.
 #'
 #' @details More details available in Brown et al. (2020) <doi:10.1111/2041-210X.13363> and vignette.
 #'
 #' @author Matilda Brown \email{matilda.brown@@utas.edu.au}
 #'
-#' @import ggplot2 e1071
-#'
-NULL
+"_PACKAGE"
